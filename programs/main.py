73ae59de7263dff45aae61de951029352537893d
MoLabo <zyarnaka@outlook.com>
2026-03-17 20:40:12 +0900

    main.pyに基本機能を追加
    目立つように文字列を表示するboxtitle()、1から始まる複数選択が可能なリストメニューを生成するmake1menu()を追加しました。
    また、make0menu()に説明を追加しました。

--- programs/main.py
+++ programs/main.py
@@ -18,6 +18,19 @@ def bigrepositoryname():
     print("##      ##                                 ##                  #######  ##       ########\n###    ###                                                    ###   ### ##          ##\n####  ####  ####   ##  ##   ####  # ###   ###   ######  ####  ##        ##          ##\n## #### ## ##  ## ## ## ## ##  ## ### ##   ##     ###  ##  ## ##        ##          ##\n##  ##  ## ####   ## ## ## ##  ## ##       ##    ###   ####   ###   ### ##          ##\n##      ##  ##### ## ## ##  ####  ##     ###### ######  #####  #######  ######## ########") # MemoriseCLI
     print()
 
+def boxtitle(text: str, dec: int=0):
+    '''
+    第1引数text: strを四角で囲みます。\n
+    半角の文字がある場合、半角の文字数を第2引数で指定してください。
+    '''
+    print("+-" + "-"*(len(text)*2-dec) + "-+")
+    print("| " + text + " |")
+    print("+-" + "-"*(len(text)*2-dec) + "-+")
+
+def minititle(text: str):
+    
+    print("=== " + text + " ===")
+
 def tomain():
     # 外部からmain.pyを呼び出すときはここから実行
     logger.info("main.pyが実行されました")
@@ -42,7 +55,11 @@ def intinput():
             return n
 
 def make0menu(*options: str) ->int:
-
+    '''
+    選択肢が0から始まるメニューを生成します。\n
+    複数選択に対応していません。\n
+    None入力は0を返すため、0をデフォルトの選択肢にしてください。
+    '''
     if len(options) < 2:
         logger.critical("構文エラー: make0menu()は2つ以上の引数を受け取ります")
         raise ValueError("構文エラー: make0menu()は2つ以上の引数を受け取ります")
@@ -61,6 +78,27 @@ def make0menu(*options: str) ->int:
         else:
             print("範囲外の値です")
 
+def make1menu(list: list) ->list:
+    '''
+    選択肢が1から始まるメニューを生成します。\n
+    make0menu()とは違い、None入力を弾き、複数選択に対応しています。
+    '''
+    while True:
+
+        for i, f in enumerate(list, start=1):
+            print(f"{i}: {f}")
+
+        c = input("> ").strip()
+
+        if not c == "":
+            try:
+                c = [int(i.strip()) for i in c.split(",")]
+            except:
+                pass
+            else:
+                return c
+            
+        print("有効値を入力してください")
 def memorize_menu():
     pass
 
